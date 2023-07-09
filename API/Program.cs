8c27e5031b79c0e956d17f86004cb6d561d12018
Wagner Bolfe <shaper@live.com>
2023-07-08 22:41:18 -0300

    Added Angular

--- API/Program.cs
+++ API/Program.cs
@@ -12,7 +12,11 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
